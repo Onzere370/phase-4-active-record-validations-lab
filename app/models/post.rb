89302a531f825852dd410083c8c24dcc392d85ec
cynthia <cynthiakagz@gmail.com>
2023-08-17 20:41:26 +0300

    finished

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, 
+    validates :post_content, length: {minimum: 250}
+    validates :post_summary, length: {minimum: 250}
+    validates :post_category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+
+    validate :clickbait_title
+
+    private
+  
+    def clickbait_title
+      unless title =~ /Won't Believe|Secret|Top \d|Guess/
+        errors.add(:title, 'is not sufficiently clickbait-y')
+      end
+    end
 end
